61180f1f0b244eeb2d82923979b5a0157f804ef3
Ruslan <nrgruslan@gmail.com>
2018-08-13 16:32:57 +0600

    Определение интерфейса в файле XML. Файлы layout

--- app/src/main/java/com/example/ruslan/viewapplication/MainActivity.java
+++ app/src/main/java/com/example/ruslan/viewapplication/MainActivity.java
@@ -9,13 +9,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // создание TextView
-        TextView textView = new TextView(this);
-        // установка текста в TextView
-        textView.setText("Hello World!");
-        // установка высоты текста
-        textView.setTextSize(22);
-        // установка визуального интерфейса для activity
-        setContentView(textView);
+
+        // устанавливаем в качестве интерфейса файл second_layout.xml
+        setContentView(R.layout.second_layout);
+
+        // получаем элемент textView
+        TextView textView = (TextView) findViewById(R.id.header);
+        // переустанавливаем у него текст
+        textView.setText("Hello Android 7!");
+
+
+//        // создание TextView
+//        TextView textView = new TextView(this);
+//        // установка текста в TextView
+//        textView.setText("Hello World!");
+//        // установка высоты текста
+//        textView.setTextSize(22);
+//        // установка визуального интерфейса для activity
+//        setContentView(textView);
     }
 }
